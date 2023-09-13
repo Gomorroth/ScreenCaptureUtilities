edbd03802f3017db79b7cb9be0204889516cb6ad
Ashley Scarlet <70315656+AshleyScarlet@users.noreply.github.com>
2023-09-13 20:01:19 +0900

    add PlayModeStateObserver

---
+++ Editor/PlayModeStateObserver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace gomoru.su.ScreenCaptureUtilities
+{
+    [InitializeOnLoad]
+    internal static class PlayModeStateObserver
+    {
+        static PlayModeStateObserver()
+        {
+            EditorApplication.playModeStateChanged += x =>
+            {
+                if (x == PlayModeStateChange.EnteredPlayMode)
+                {
+                    var camera = Camera.main;
+                    var c = camera.gameObject.GetComponent<ScreenCapture>();
+                    if (c == null)
+                    {
+                        camera.gameObject.AddComponent<ScreenCapture>();
+                    }
+                }
+            };
+        }
+    }
+}
