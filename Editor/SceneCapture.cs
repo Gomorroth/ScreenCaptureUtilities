c3d0baf634f82d60a46303b65fef4fe5c7fbdb2e
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-09-13 15:02:06 +0900

    add SceneCapture [WIP]

---
+++ Editor/SceneCapture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace gomoru.su.ScreenCaptureUtilities 
+{
+    [InitializeOnLoad]
+    internal static class SceneCapture
+    {
+        static SceneCapture()
+        {
+            SceneView.duringSceneGui += scene =>
+            {
+                Handles.BeginGUI();
+                try
+                {
+                    var rect = scene.position;
+
+                    var boxRect = default(Rect);
+                    boxRect.x = 5;
+                    boxRect.y = 5;
+                    boxRect.width = 200;
+                    boxRect.height = EditorGUIUtility.singleLineHeight;
+
+                    GUI.Box(boxRect, GUIContent.none, GUI.skin.window);
+                }
+                finally
+                {
+                    Handles.EndGUI();
+                }
+            };
+        }
+
+    }
+}
